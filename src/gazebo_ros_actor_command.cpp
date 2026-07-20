0479cfd0feef0472cc3b5e6934ae2e4b3b900e63
Miron Kolodziejczyk <miron.kolodziejczyk@piap.lukasiewicz.gov.pl>
2026-07-20 12:50:23 +0200

    update

--- src/gazebo_ros_actor_command.cpp
+++ src/gazebo_ros_actor_command.cpp
@@ -353,9 +353,9 @@ void GazeboRosActorCommand::PostUpdate(const gz::sim::UpdateInfo &_info,
     
     // auto actorpose = _ecm.Component<gz::sim::components::WorldPose>(this->actorEntity_);
 
-    // auto worldPoseComp = _ecm.Component<gz::sim::components::WorldPose>(this->actorEntity_);
-    auto trajPoseComp = _ecm.Component<gz::sim::components::TrajectoryPose>(this->actorEntity_);
-    auto actorPose = trajPoseComp->Data();
+    auto worldPoseComp = _ecm.Component<gz::sim::components::WorldPose>(this->actorEntity_);
+    // auto trajPoseComp = _ecm.Component<gz::sim::components::TrajectoryPose>(this->actorEntity_);
+    auto actorPose = worldPoseComp->Data();
     auto currentPose = actorPose;
 
     // gzmsg << "actor pose: " << actorpose->Data().Pos().X() << std::endl;
